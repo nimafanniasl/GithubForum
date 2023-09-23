4ec601b2b3e4d44e6be3895d429d5293129c0344
Nima <smartnimaco@gmail.com>
2023-09-23 21:44:13 +0330

    Added some stuff again

--- generate_post.py
+++ generate_post.py
@@ -1,3 +1,9 @@
+import os
+import sys
+from datetime import datetime
+
+# Template for the HTML file
+template = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,3 +37,28 @@
     </footer>
 </body>
 </html>
+"""
+
+# Replace placeholders with actual data
+issue_number = sys.argv[1]
+post_title = sys.argv[2]
+post_author = sys.argv[3]
+post_content = sys.argv[4]
+
+html_content = template.replace("{POST-TITLE}", post_title)
+html_content = html_content.replace("{POST-AUTHOR}", post_author)
+html_content = html_content.replace("{POST-DATE}", datetime.now().strftime("%Y-%m-%d"))
+html_content = html_content.replace("{POST-NUMBER}", issue_number)
+html_content = html_content.replace("{POST-CONTENT}", post_content)
+
+
+# Define the output directory
+output_directory = "posts"
+
+# Ensure the output directory exists
+os.makedirs(output_directory, exist_ok=True)
+
+# Save the HTML content to a file
+output_file_path = os.path.join(output_directory, f"{issue_number}.html")
+with open(output_file_path, "w") as f:
+    f.write(html_content)
